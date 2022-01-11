52d2d64e25b37ba9c40ffef44f8688f58f027da8
Dorodong96 <dk143496@gmail.com>
2022-01-12 08:07:15 +0900

    Radix Sort

--- radix_sort.py
+++ radix_sort.py
@@ -12,20 +12,20 @@ def prefix_sum(array):
     return array
 
 
-def radixsort(1, base=10):
-    passes = int(log10(max(1)) + 1)
-    output = [0] * len(1)
+def radixsort(l, base=10):
+    passes = int(log10(max(l)) + 1)
+    output = [0] * len(l)
 
     for pos in range(passes):
         count = [0] * base
 
-        for i in 1:
+        for i in l:
             digit = get_digit(i, base, pos)
             count[digit] += 1
 
-        ocunt = prefix_sum(count)
+        count = prefix_sum(count)
 
-        for i in reversed(1):
+        for i in reversed(l):
             digit = get_digit(i, base, pos)
             count[digit] -= 1
             new_pos = count[digit]
@@ -34,12 +34,13 @@ def radixsort(1, base=10):
         l = list(output)
     return output
 
+
 if __name__ == '__main__':
     r_list = []
     r_list = [randint(1, 99999) for x in range(100)]
     print(" < Before Sort > ")
     print(r_list)
 
-    sorted = radixsort(r_list)
+    r_sorted = radixsort(r_list)
     print(" < After Sort > ")
-    print(sorted)
+    print(r_sorted)
